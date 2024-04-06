2784abacdd25ac04b56185707ed86d2706f0a678
Janne Soikkeli <pullaposki@windowslive.com>
2024-04-06 13:22:30 +0300

    added operators

--- main.c
+++ main.c
@@ -3,14 +3,39 @@
 
 int main(){
     double num1, num2;
-    
-    printf("Enter first number: ");
+    char ope;
+
+    printf("Enter a number: ");
     scanf("%lf", &num1);
 
-    printf("Enter second number: ");
-    scanf("%lf", &num2);
+    printf("Enter operator: ");
+    scanf(" %c", &ope);
 
-    printf("Sum of %f and %f is %f\n", num1, num2, num1 + num2);
+    printf("Enter another number: ");
+    scanf("%lf", &num2);
+    
+    switch (ope)
+	{
+	    case '+':
+            printf("%lf\n", num1 + num2);
+		    break;
+
+	    case '-':
+            printf("%lf\n", num1 - num2);
+		    break;
+
+        case '*':
+            printf("%lf\n", num1 * num2);
+            break;
+
+        case '/':
+            printf("%lf\n", num1 / num2);
+            break;
+	    
+	    default:
+            printf("Invalid operator\n");
+		    break;
+	}
 
     return 0;
 }
